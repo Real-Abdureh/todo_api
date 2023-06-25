7d6c72b0b0218f0ce810a3b5d0fa984b7e79d3cc
ABDULRAHMAN ABBAS <abdouliabbas75@gmail.com>
2023-06-25 08:43:18 +0100

    finished

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,9 +1,10 @@
 # todo/todo_api/urls.py : API urls.py
 from django.urls import path, include
 from .views import (
-    TodoListApi,
+    TodoListApi, TodoDetail
 )
 
 urlpatterns = [
     path('api', TodoListApi.as_view()),
+    path('api/<int:todo_id>/', TodoDetail.as_view()),
 ]
--- todo_api/views.py
+++ todo_api/views.py
@@ -22,6 +22,8 @@ class TodoListApi(APIView):
         todos = Todo.objects.filter(user = request.user.id)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data , status=status.HTTP_200_OK)
+    
+    
     def post(self, request,*args, **kwargs):
 
         '''
@@ -96,6 +98,31 @@ class TodoDetail(APIView):
             'completed':request.data.get('get'),
             'user': request.user.id
         }
+        serializer = TodoSerializer(instance= todo_instance, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    #delete
+    def delete(self, request, todo_id, *args, **kwargs):
+
+        '''
+        Deletes the todo items with the given todo_id if exist
+
+        '''
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"res": "obeject with todo does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        todo_instance.delete()
+        return Response(
+            {"res": "object delete!"},
+            status=status.HTTP_200_OK
+        )
+    
     
 
     
